04e1ca0841a16c74fd606c28cf9a7d834f863d57
Chidanand AV <chidanandav55@gmail.com>
2022-06-08 15:45:31 +0530

    Day 1

---
+++ guess-game/src/main.rs
@@ -0,0 +1,39 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+
+    println!("welcome to guessing game!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    //println!("The secret number is {}", secret_number);
+
+    loop {
+
+        println!("Please input your guess number...");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("you guessed -> {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!!"),
+            Ordering::Greater => println!("Too big!!"),
+            Ordering::Equal => { 
+                println!("you win!!");
+                break;
+            }
+        }
+    }
+}
